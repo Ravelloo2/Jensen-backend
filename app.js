9d7d76a7ae9e7d7ae2cc89daf5ac0a9bbfb4478e
Eliaz Ravello <eliaz@telia.com>
2022-01-17 09:43:46 +0000

    Working

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const credentials = {secretUser:"user" , secretPassword:"password"}
 
 const cors = require('cors')
-
+const bodyParser = ('body-parser')
 const express = require('express')
+const jwt = require('jsonwebtoken')
 
 const app = express()
 
+app.use(function (req, res, next) {
+    res.setHeader('Content-Security-Policy', "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'");
+    next();
+});
+
+app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 
 app.get('/', (req, res) => {
@@ -16,11 +23,37 @@ app.get('/', (req, res) => {
         .toString().split(':')
         if(user === credentials.secretUser && password === credentials.secretPassword) {
             res.status(200).send({"STATUS":"SUCCESS"})
+            console.log("Logged in")
         } else {
             res.set('WWW-Authenticate', 'Basic realm="Access to Index"')
             res.status(401).send('Unauthorized Access')
         }
 })
+
+app.get("/health", (req, res) => {
+    headers = {"http_status":200, "cache-control": "no-cache"}
+    body = {"status": "available"}
+})
+
+app.post('/authorize', (req, res) => {
+    let user = req.body.user;
+    let password = req.body.password
+    console.log(`User ${user}`)
+    console.log(`password ${password}`)
+
+    if (user === credentials.secretUser && password === credentials.secretPassword) {
+        console.log("Authorized")
+        const token = jwt.sign({
+            data: 'foobar'
+        }, 'your-secret-key-here', { expiresIn: 60 * 60 });
+
+        console.log(token)
+        res.status(200).send(token) 
+    } else {
+        console.log("Not authorized")
+        res.status(200).send({"STATUS":"FAILURE"})
+    }
+});
 
 app.listen(3000, () => {
     console.log("Listening on port 3000")
